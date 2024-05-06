7db6005b61e56217fb68c3ec1393d7246241a664
Alex Good <alex@memoryandthought.me>
2024-05-06 22:58:09 +0100

    Set base URL for playground based on GITHUB_PAGES env var

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react"
 import wasm from "vite-plugin-wasm"
 import topLevelAwait from "vite-plugin-top-level-await"
 
+let base = "/"
+if (process.env.GITHUB_PAGES) {
+  base = "/automerge-prosemirror/"
+}
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [wasm(), topLevelAwait(), react()],
 })
