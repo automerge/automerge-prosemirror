5efc7013c4bac0e837abf8f7831a1fe1f7652a81
Alex Good <alex@memoryandthought.me>
2024-05-24 10:16:09 +0100

    fix eslint complaints for react example

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -29,6 +29,7 @@ window.handle = handle // we'll use this later for experimentation
 // @ts-expect-error -- we put the handle and the repo on window so you can experiment with them from the dev tools
 window.repo = repo
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RepoContext.Provider value={repo}>
